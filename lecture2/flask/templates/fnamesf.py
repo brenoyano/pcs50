f37a35b3b8e13444192ef45aebfbe8a7e064e1b3
brenoyano <brenoyano@gmail.com>
2018-07-11 09:08:38 -0300

    More Flask file yey!

---
+++ lecture2/flask/templates/fnamesf.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    names = ["Alice", "Bob", "Charlie"]
+    return render_template("fnamef_index.html", names=names)
